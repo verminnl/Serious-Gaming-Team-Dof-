adea365e2c54d8bf5bd8c3944243361dc992ab5c
Hai feng Lin <haifenglin13@gmail.com>
2016-03-22 23:47:00 +0100

    Edit: Elevator transportation between each floor added with a trigger box. Removed the button press interaction. Set up start points at each floor for the elevators

--- Bakkie doen/Assets/Scripts/LoadNewArea.cs
+++ Bakkie doen/Assets/Scripts/LoadNewArea.cs
@@ -4,13 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class LoadNewArea : MonoBehaviour
 {
-
     public string levelToLoad;
+
+    public string exitPoint;
+
+    private PlayerController thePlayer;
 
     // Use this for initialization
     void Start()
     {
-
+        thePlayer = FindObjectOfType<PlayerController>();
     }
 
     // Update is called once per frame
@@ -21,9 +24,10 @@ public class LoadNewArea : MonoBehaviour
     
     public void OnTriggerStay2D(Collider2D other)
     {
-        if (other.gameObject.name == "Player" && Input.GetKeyDown(KeyCode.K))
+        if (other.gameObject.name == "Player")
         {
             SceneManager.LoadScene(levelToLoad);
+            thePlayer.startPoint = exitPoint;
         }
     }
 }
--- Bakkie doen/Assets/Scripts/PlayerStartPoint.cs
+++ Bakkie doen/Assets/Scripts/PlayerStartPoint.cs
@@ -8,14 +8,20 @@ public class PlayerStartPoint : MonoBehaviour {
 
     public Vector2 startDirection;
 
+    public string pointName;
+
 	// Use this for initialization
 	void Start () {
         thePlayer = FindObjectOfType<PlayerController>();
-        thePlayer.transform.position = transform.position;
-        thePlayer.lastMove = startDirection;
 
-        theCamera = FindObjectOfType<CameraController>();
-        theCamera.transform.position = new Vector3(transform.position.x, transform.position.y, theCamera.transform.position.z);
+        if (thePlayer.startPoint == pointName)
+        {
+            thePlayer.transform.position = transform.position;
+            thePlayer.lastMove = startDirection;
+
+            theCamera = FindObjectOfType<CameraController>();
+            theCamera.transform.position = new Vector3(transform.position.x, transform.position.y, theCamera.transform.position.z);
+        }
 	}
 	
 	// Update is called once per frame
