f921fd44093e2305fdd571e654ecd2cb37dcf0db
Hai feng Lin <haifenglin13@gmail.com>
2016-04-11 16:53:06 +0200

    Edit: Can spawn a random enemy in the minigame, still need to fix it so it spawns on a random x-coordinate

---
+++ Bakkie doen/Assets/Scripts/Red minigame scripts/EnemyDestroyer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDestroyer : MonoBehaviour {
+
+    public GameObject enemyDestructionPoint;
+
+	// Use this for initialization
+	void Start () {
+        enemyDestructionPoint = GameObject.Find("Enemy Destruction Point");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < enemyDestructionPoint.transform.position.y)
+        {
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
+        }
+	}
+}
---
+++ Bakkie doen/Assets/Scripts/Red minigame scripts/EnemySpawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemy;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    public float distanceBetweenMin;
+    public float distanceBetweenMax;
+
+    private int enemySelector;
+
+    public ObjectPooler[] theObjectPools;
+
+    private float minX;
+    public Transform maxXPoint;
+    private float maxX;
+
+	// Use this for initialization
+	void Start () {
+        minX = transform.position.x;
+        maxX = maxXPoint.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < generationPoint.position.y)
+        {
+            distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+
+            enemySelector = Random.Range(0, theObjectPools.Length);
+
+            transform.position = new Vector3(transform.position.x, transform.position.y + distanceBetween, transform.position.z);
+
+            //Instantiate(enemy, transform.position, transform.rotation);
+            GameObject newEnemy = theObjectPools[enemySelector].GetPooledObject();
+
+            newEnemy.transform.position = transform.position;
+            if (theObjectPools[enemySelector].GetPooledObject().name == "meteorite(Clone)")
+            {
+                newEnemy.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+            }
+            else
+            {
+                newEnemy.transform.rotation = transform.rotation;
+            }
+            newEnemy.SetActive(true);
+        }
+	}
+}
