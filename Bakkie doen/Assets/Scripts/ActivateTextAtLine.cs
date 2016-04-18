50e8fab4947b91a130e07cd87a87e6ed629e95c0
Hai feng Lin <haifenglin13@gmail.com>
2016-04-18 10:04:27 +0200

    Edit: Changed the character walking animation for the player

--- Bakkie doen/Assets/Scripts/ActivateTextAtLine.cs
+++ Bakkie doen/Assets/Scripts/ActivateTextAtLine.cs
@@ -3,25 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
+/// <summary>
+/// Activates a certain line from a dialogue
+/// </summary>
 public class ActivateTextAtLine : MonoBehaviour {
-
+    //Line where the dialogue will start
     public int startLine;
+    //Line where the dialogue will end
     public int endLine;
-
+    //Current textbox manager in the game
     public TextBoxManager theTextBox;
-
+    //List with the lines for the dialogue
 	private List<string> theScript = new List<string>();
-
+    //Checks if the dialogue requires button press to activate
     public bool requireButtonPress;
+    //Checks if trigger is waiting for a button press
     private bool waitForPress;
-
+    //Checks if the trigger should be destroted when activated
     public bool destroyWhenActivated;
+    //Checks if it's the narrator's dialogue
     public bool isNarratorTrigger;
 
 	// Use this for initialization
 	void Start () {
         theTextBox = FindObjectOfType<TextBoxManager>();
-		//theScript = DialogueClass.Instance.GetDialogueForNPC (gameObject.name).ToList();
+
+        //Gets the dialogue for the current connected gameobject and adds it to theScript
         if (isNarratorTrigger)
         {
             var h = DialogueClass.Instance.GetNarratorDialogue(gameObject.name);
@@ -42,6 +49,7 @@ public class ActivateTextAtLine : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //When player is in the trigger and presses on the "Enter" key, activate the dialogue and stops player movement
         if (waitForPress && Input.GetKeyUp(KeyCode.Return))
         {
 			theTextBox.ReloadScript(theScript.ToArray());
@@ -56,16 +64,6 @@ public class ActivateTextAtLine : MonoBehaviour {
             theTextBox.isNPCDialogue = true;
             waitForPress = false;
 
-
-            //var test = new System.Collections.Generic.Dictionary<NPCCLASS, System.Collections.Generic.List<string>>();
-
-            //var listwithdialog = test[REDNPC];
-
-            //loop door die lijst heen enzo
-
-            //if(WELKE REGEL VAN DE LIST<STRING> IS NET VOORGELEZEN == HET EIND VAN DE LIST)
-            //CLOSE DIALOG LOAD NEW LEVEL
-
             if (destroyWhenActivated)
             {
                 Destroy(gameObject);
@@ -73,6 +71,10 @@ public class ActivateTextAtLine : MonoBehaviour {
         }
 	}
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="other"></param>
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name == "Player")
@@ -100,6 +102,10 @@ public class ActivateTextAtLine : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="other"></param>
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.name == "Player")
