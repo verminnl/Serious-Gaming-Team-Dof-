9d6e57f07f36b11e6278a97c7fb17e1bf8175999
Hai feng Lin <haifenglin13@gmail.com>
2016-04-20 14:20:08 +0200

    Edit: Added editable values of the loading screen into the script for in the Inspector in Unity, code clean-up

--- Bakkie doen/Assets/Scripts/GameController.cs
+++ Bakkie doen/Assets/Scripts/GameController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Keeps the game in check
@@ -9,22 +9,34 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour {
     //Time that has passed since the start of the game
     private static float playedTime;
-
+    //NPC that the player is talking to
     public NPCController theNPC;
-
+    //A minigame type
     private string minigameType;
-
+    //Counts the time after finishing dialogue with an NPC
     private float npcMinigameStartCounter;
-
+    //Current player
     private PlayerController thePlayer;
-
+    //Current camera
     private CameraController theCamera;
-
-    private bool chosenMinigame = false;
-
+    //List with all the NPCs in the game
     private List<NPCClass> npcList = new List<NPCClass>();
-
+    //Loading screen for the game
     public GameObject theLoadingTransition;
+    //Time for the loading screen to be active
+    private float loadingScreenTime;
+    //Time for the loading screen to appear
+    private float startLoading;
+
+    //Sets the loadingScreenTime and the startLoading variables to the values that has been given in the Inspector
+    //at the LoadingTransition script
+    void Awake()
+    {
+        theLoadingTransition.SetActive(true);
+        loadingScreenTime = FindObjectOfType<LoadingTransition>().loadingTime;
+        startLoading = FindObjectOfType<LoadingTransition>().timeToStartLoadingScreen;
+        theLoadingTransition.SetActive(false);
+    }
 
 	// Use this for initialization
 	void Start () {
@@ -37,77 +49,94 @@ public class GameController : MonoBehaviour {
         //Counts the time that has passed
         playedTime += Time.deltaTime;
 
+        //When the player is talking to an NPC, set minigameType to the colorType of the NPC
         if (theNPC != null)
         {
             minigameType = theNPC.colorType;
+
+            //When finished talking to an NPC, start the loading screen and start a time counter
             if (theNPC.dialogueFinished)
             {
                 theLoadingTransition.SetActive(true);
                 npcMinigameStartCounter += Time.deltaTime;
-                if (npcMinigameStartCounter > 2)
+
+                //When the time counter is higher than the loadingScreenTime, start the minigame based on minigameType
+                if (npcMinigameStartCounter > loadingScreenTime)
                 {
                     ActivateMinigame(minigameType);
-                    chosenMinigame = true;
                 }
             }
         }
+
         TimePassed();
 
-        if (Mathf.FloorToInt(playedTime) == 18)
+        //Start loading screen if player hasn't talked to an NPC for {startLoading} seconds and
+        //activate minigame after {loadingScreenTime} seconds
+        if (Mathf.FloorToInt(playedTime) == startLoading)
         {
             Debug.Log("-----------------------------------------------------");
             theLoadingTransition.SetActive(true);
         }
-        else if (Mathf.FloorToInt(playedTime) == 20)
+        else if (Mathf.FloorToInt(playedTime) == startLoading + loadingScreenTime)
         {
             ActivateMinigame(npcList[randomMinigameColor()].type);
-            chosenMinigame = true;
         }
 	}
 
+    /// <summary>
+    /// Gets a random NPC index from npcList
+    /// </summary>
+    /// <returns>An index of a random NPC</returns>
     int randomMinigameColor()
     {
-        int npc = Random.Range(0, DialogueClass.Instance.GetNPCDictionaryLength());
+        int npc = Random.Range(0, npcList.Count);
         Debug.Log("Random number: " + npc);
         return npc;
     }
 
+    /// <summary>
+    /// Activates a minigame based on the type
+    /// </summary>
+    /// <param name="type">Type of the minigame</param>
     public void ActivateMinigame(string type)
     {
-        if (!chosenMinigame)
+        switch (type)
         {
-            switch (type)
-            {
-                case "red":
-                    theCamera.isLevelCamera = false;
-                    thePlayer.inMinigame = true;
-                    SceneManager.LoadScene("Red minigame");
-                    break;
-                case "blue":
-                    theCamera.isLevelCamera = false;
-                    thePlayer.inMinigame = true;
-                    SceneManager.LoadScene("Blue minigame");
-                    break;
-                case "green":
-                    theCamera.isLevelCamera = false;
-                    thePlayer.inMinigame = true;
-                    SceneManager.LoadScene("Green minigame");
-                    break;
-                case "yellow":
-                    theCamera.isLevelCamera = false;
-                    thePlayer.inMinigame = true;
-                    SceneManager.LoadScene("Yellow minigame");
-                    break;
-            }            
+            case "red":
+                theCamera.isLevelCamera = false;
+                thePlayer.inMinigame = true;
+                SceneManager.LoadScene("Red minigame");
+                break;
+            case "blue":
+                theCamera.isLevelCamera = false;
+                thePlayer.inMinigame = true;
+                SceneManager.LoadScene("Blue minigame");
+                break;
+            case "green":
+                theCamera.isLevelCamera = false;
+                thePlayer.inMinigame = true;
+                SceneManager.LoadScene("Green minigame");
+                break;
+            case "yellow":
+                theCamera.isLevelCamera = false;
+                thePlayer.inMinigame = true;
+                SceneManager.LoadScene("Yellow minigame");
+                break;
         }
     }
 
+    /// <summary>
+    /// Adds the NPCs in the game to npcList
+    /// </summary>
+    /// <param name="theNPC">NPC that should be added to the list</param>
     public void AddNPCToList(NPCClass theNPC)
     {
         npcList.Add(theNPC);
-        Debug.Log("NPC added");
     }
 
+    /// <summary>
+    /// Displays the time that has passed since the start of the game in the console
+    /// </summary>
     public void TimePassed()
     {
         Debug.Log("Amount of time played: " + playedTime);
--- Bakkie doen/Assets/Scripts/LoadingTransition.cs
+++ Bakkie doen/Assets/Scripts/LoadingTransition.cs
@@ -3,10 +3,14 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class LoadingTransition : MonoBehaviour {
-
-    //public GameObject theLoadingScreen;
+    //Image for the loading screen
     public Image theLoadingImage;
+    //Speed for the rotation of the image
     public float imageRotateSpeed;
+    //How long the loading screen should last
+    public float loadingTime;
+    //When to start a loading screen based on time in seconds
+    public float timeToStartLoadingScreen;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +19,7 @@ public class LoadingTransition : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //Rotates the image on the loading screen
         theLoadingImage.rectTransform.Rotate(new Vector3(0, imageRotateSpeed * Time.deltaTime, 0));
 	}
 }
