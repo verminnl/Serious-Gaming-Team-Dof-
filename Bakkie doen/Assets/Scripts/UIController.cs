86950c98acf2f63a9634f886b0733eaceabc6be0
Hai feng Lin <haifenglin13@gmail.com>
2016-04-20 16:41:43 +0200

    Edit: Added an area to the canvas where it shows the current area that the player is in

---
+++ Bakkie doen/Assets/Scripts/UIController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+    //The area box in the game
+    public Text areaBoxText;
+    //Checks if the areaBox exists
+    private static bool areaBoxExists;
+
+	// Use this for initialization
+    void Start() {
+        if (!areaBoxExists)
+        {
+            areaBoxExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+    void Update() {
+        areaBoxText.text = SceneManager.GetActiveScene().name;
+	}
+}
