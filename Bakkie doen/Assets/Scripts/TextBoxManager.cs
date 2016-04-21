58ee50e81773241d884311c258ad56bba510cbcd
Hai feng Lin <haifenglin13@gmail.com>
2016-04-21 11:34:21 +0200

    Edit: Created a basic UI for the game for each scenes

--- Bakkie doen/Assets/Scripts/TextBoxManager.cs
+++ Bakkie doen/Assets/Scripts/TextBoxManager.cs
@@ -13,7 +13,7 @@ public class TextBoxManager : MonoBehaviour {
     public int currentLine;
     public int endAtLine;
 
-    public PlayerController player;
+    private PlayerController player;
 
     public bool isActive;
 
--- Bakkie doen/Assets/Scripts/UIController.cs
+++ Bakkie doen/Assets/Scripts/UIController.cs
@@ -4,26 +4,30 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class UIController : MonoBehaviour {
-    ////The area box in the game
-    //public Text areaBoxText;
-    ////Checks if the areaBox exists
-    //private static bool areaBoxExists;
+    //The area box in the game
+    public Text areaBoxText;
+    //Checks if the areaBox exists
+    private static bool areaBoxExists;
 
-    //// Use this for initialization
-    //void Start() {
-    //    if (!areaBoxExists)
-    //    {
-    //        areaBoxExists = true;
-    //        DontDestroyOnLoad(transform.gameObject);
-    //    }
-    //    else
-    //    {
-    //        Destroy(gameObject);
-    //    }
-    //}
-	
-    //// Update is called once per frame
-    //void Update() {
-    //    areaBoxText.text = SceneManager.GetActiveScene().name;
-    //}
+    // Use this for initialization
+    void Start()
+    {
+        //areaBoxText.text = SceneManager.GetActiveScene().name;
+
+        //if (!areaBoxExists)
+        //{
+        //    areaBoxExists = true;
+        //    DontDestroyOnLoad(transform.gameObject);
+        //}
+        //else
+        //{
+        //    Destroy(gameObject);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
