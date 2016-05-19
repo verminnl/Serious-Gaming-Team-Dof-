0d62e15b81bcfb3ff7ce69fef7069031a3fe9641
Dominique te Riele <dominiqueteriele@hotmail.com>
2016-05-19 14:32:04 +0200

    New protocol

--- Bakkie doen/Assets/Scripts/Database/BackEndCommunicator.cs
+++ Bakkie doen/Assets/Scripts/Database/BackEndCommunicator.cs
@@ -20,7 +20,7 @@ public class BackEndCommunicator {
     }
 
     public string Protocol = "http://";
-    public string URL = "verminnl.github.io/Serious-Gaming-Team-Dofe/Database";
+    public string URL = "dodo.cah.onl";
     private BackEndCommunicator()
     {
         //Set specific stuff here if needed
