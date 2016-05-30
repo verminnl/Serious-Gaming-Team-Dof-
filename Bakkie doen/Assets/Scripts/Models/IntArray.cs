ba39100ea6d0f104b9cd0fe9c29456d65cb3b9b2
Dominique te Riele <dominiqueteriele@hotmail.com>
2016-05-30 14:36:19 +0200

    Comments in intArray

--- Bakkie doen/Assets/Scripts/Models/IntArray.cs
+++ Bakkie doen/Assets/Scripts/Models/IntArray.cs
@@ -1,5 +1,8 @@
 ﻿/// <summary>
-/// 
+/// Special Object containt an int array.
+/// This was required to use decode json.
+/// json requires an object to decode to.
+/// An int[] is a primitive type and not object, this class however is an object.
 /// </summary>
 public class IntArray
 {
