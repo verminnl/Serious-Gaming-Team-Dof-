4f856dfd52d7a6c351a6b23e6138c817ab7b4905
Hai feng Lin <haifenglin13@gmail.com>
2016-06-02 13:22:31 +0200

    Edit: UI updated with the numbers of NPC found for a player

---
+++ Bakkie doen/Assets/Scripts/TotalFoundNpcsBoxController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// Shows the number of found NPCs on the UI of the player
+/// </summary>
+public class TotalFoundNpcsBoxController : MonoBehaviour {
+    public Text textbox;
+
+	// Use this for initialization
+	void Start () {
+        textbox.text = "Gevonden collega's: " + DataTracking.playerData.FoundPlayers.intList.Length + "/" + DataTracking.npcData.Count;
+	}
+}
