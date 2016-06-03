276c5e4c0536f5bc269875518eba1d62a955a4e7
Dominique te Riele <dominiqueteriele@hotmail.com>
2016-06-03 11:09:26 +0200

    Added collision to npc, space bar press on end of tutorial

--- Bakkie doen/Assets/Scripts/Game tutorial/TutorialEnd.cs
+++ Bakkie doen/Assets/Scripts/Game tutorial/TutorialEnd.cs
@@ -9,24 +9,15 @@ public class TutorialEnd : MonoBehaviour {
     public Image backgroundImage;
     //Textbox for the end screen
     public Text theText;
-    //Name of the player
-    public string namePlayer;
-    //Time that the screen will be active
-    public float activeTime;
-    //Time that the screen is active
-    private float timer;
 
 	// Use this for initialization
 	void Start () {
         theText.text = theText.text + DataTracking.playerData.FirstName + "!";
-        timer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        timer = timer + Time.deltaTime;
-
-        if (timer > activeTime)
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             BackEndCommunicator.Instance.EndGameSave(DataTracking.playerData.PlayerID, 0, DataTracking.playerData.SessionID, "", false);
             DataTracking.resetGame();
