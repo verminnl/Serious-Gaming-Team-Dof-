c8588929399579857032843559d8ad98d73fdb6b
Hai feng Lin <haifenglin13@gmail.com>
2016-06-10 14:30:43 +0200

    Edit: Code comments added to the NPC and NPCPlacer scripts

--- Bakkie doen/Assets/Scripts/NPC/NPC.cs
+++ Bakkie doen/Assets/Scripts/NPC/NPC.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Controls the behaviour and the details of an NPC
+/// </summary>
 public class NPC : MonoBehaviour {
+    //Data of this NPC
     public AvatarData avatar;
+    //Dialogue box in the game
     public GameObject textBox;
+    //Player in the game
     public PlayerController player;
+    //Checks if the dialogue box is active
     public bool textBoxActive;
+    //Textbox for the dialogue
     public Text textBoxText;
-
+    //Current line of the dialogue
     private int lineCounter;
+    //Game controller of the game
     public GameController gameController;
+    //Check if the player is in the triggerbox of this NPC
     private bool playerInTriggerBox;
    
     // Use this for initialization
@@ -20,6 +30,7 @@ public class NPC : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //Activates the dialogue if the player is near this NPC and he presses on the {Enter key or keypadenter}
         if (playerInTriggerBox)
         {
             if (Input.GetKeyUp(KeyCode.KeypadEnter) || Input.GetKeyUp(KeyCode.Return))
@@ -27,6 +38,7 @@ public class NPC : MonoBehaviour {
                 ActivateDialogue();
             }
         }
+        //Goes through the dialogue when the dialogue box is active and when the player presses on the {Enter key or keypadenter}
         if (textBoxActive)
         {
             if(lineCounter == 0)
@@ -50,7 +62,12 @@ public class NPC : MonoBehaviour {
             }
         }
     }
-    
+
+
+    /// <summary>
+    /// Activates when this gameobject collides with another gameobject
+    /// </summary>
+    /// <param name="other">The gameobject that this gameobject collides with</param>
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name == "Player")
@@ -59,6 +76,10 @@ public class NPC : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Activates when this gameobject stops colliding with another gameobject
+    /// </summary>
+    /// <param name="other">The gameobject that this gameobject was colliding with</param>
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.name == "Player")
@@ -67,6 +88,9 @@ public class NPC : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Activates the dialogue box
+    /// </summary>
     void ActivateDialogue()
     {
         textBox.SetActive(true);
--- Bakkie doen/Assets/Scripts/NPC/NPCPlacer.cs
+++ Bakkie doen/Assets/Scripts/NPC/NPCPlacer.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Places NPCs on some given positions of gameobjects
+/// </summary>
 public class NPCPlacer : MonoBehaviour {
+    //Locations for NPCs to be placed
     public GameObject[] locations;
+    //A random NPC from the database
     public AvatarData randomNPC;
     
     // Use this for initialization
     void Start () {
+        //Places a random NPC on each of the given locations
         foreach (GameObject location in locations)
         {
             randomNPC = DataTracking.npcData[Random.Range(0, DataTracking.npcData.Count - 1)];
@@ -13,9 +19,4 @@ public class NPCPlacer : MonoBehaviour {
             location.GetComponent<SpriteRenderer>().sprite = randomNPC.NPCSprite;
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
 }
